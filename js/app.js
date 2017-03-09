72b7c8e018299b99ff186e22f56282799532352e
Dan Lin <dlin2102@gmail.com>
2017-03-09 00:15:07 -0500

    commit 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", [
+      "ui.router",
+      "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunc
+    ])
+
+  function RouterFunc($stateProvider) {
+    $stateProvider
+      .state("entriesIndex", {
+        url: "/entries",
+        templateUrl: "js/entries/index.html",
+        controller: "EntriesIndexCtrl",
+        controllerAs: "indexVm"
+      })
+    
+  }
+
 })();
